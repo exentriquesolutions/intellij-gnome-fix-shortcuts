7280fa55c9288ebd8ae260ecac2ee4fe7673f0e4
rses <rs@exentriquesolutions.com>
2020-11-22 14:26:12 +0000

    Script to disable clashing Gnome keymaps

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# intellij-gnome-fix-shortcuts
+## Remove Gnome Shortcuts to work with IntelliJ
+
+If you're used to Windows and XWin IntelliJ shortcuts
+use the script `fix-shortcuts.sh` to remove Gnome
+default shortcuts that clash with it.
+
+Adapted from [this original fix-shortcuts.sh](https://gist.github.com/JonasGroeger/94cfa1071fa12572f465)
---
+++ fix-shortcuts.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly KEYS=(
+    "/org/gnome/settings-daemon/plugins/media-keys/screensaver"
+    "/org/gnome/settings-daemon/plugins/media-keys/terminal"
+    "/org/gnome/desktop/wm/keybindings/toggle-shaded"
+    "/org/gnome/desktop/wm/keybindings/begin-move"
+    "/org/gnome/desktop/wm/keybindings/begin-resize"
+    "/org/gnome/desktop/wm/keybindings/panel-main-menu"
+    "/org/gnome/desktop/wm/keybindings/switch-to-workspace-down"
+    "/org/gnome/desktop/wm/keybindings/switch-to-workspace-up"
+    "/org/gnome/desktop/wm/keybindings/switch-to-workspace-left"
+    "/org/gnome/desktop/wm/keybindings/switch-to-workspace-right"
+    # To disable resetting a value, just comment out the line
+)
+readonly DISABLED_VALUE="['']"
+
+for key in "${KEYS[@]}"; do
+    dconf write "$key" "$DISABLED_VALUE"
+done
